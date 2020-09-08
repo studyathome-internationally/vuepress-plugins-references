97fc723a0ed8f112ac821ad268537a08868b8b33
Alija Sabic <sabic.alija@gmail.com>
2020-09-09 01:56:04 +0200

    vuepress-plugin-figure-references: add custom figure_wrapper, add new options and pass-through handling

--- packages/vuepress-plugin-figure-references/index.js
+++ packages/vuepress-plugin-figure-references/index.js
@@ -1,10 +1,13 @@
 const plugin = (options, ctx) => {
   const opts = Object.assign({}, plugin.defaults, options);
+  opts.options = Object.assign({}, require("markdown-it-figure-references").defaults, options.options);
   return {
     name: "vuepress-plugin-figure-references",
     extendMarkdown: (md) => {
-      md.use(require("markdown-it-figure-references"), opts);
+      md.use(require("markdown-it-figure-references"), opts.options);
       md.renderer.rules.figure_reference_list_open = figure_reference_list_open_renderer(opts);
+      const defaultRenderer = md.renderer.rules.figure_wrapper;
+      md.renderer.rules.figure_wrapper = figure_wrapper_renderer(opts, defaultRenderer);
     },
   };
 };
@@ -12,15 +15,39 @@ const plugin = (options, ctx) => {
 function figure_reference_list_open_renderer(opts) {
   return (tokens, idx /* , options, env, self */) => {
     const token = tokens[idx];
-    const title = opts.listTitle
-      ? `<h2 id="list-of-figures"><a href="#list-of-figures" class="header-anchor">#</a>${opts.listTitle}</h2>\n`
+    const title = opts.options.listTitle
+      ? `<h2 id="list-of-figures">${
+          opts.listHeaderAnchor ? `<a href="#list-of-figures" class="header-anchor">#</a>` : ""
+        }${opts.options.listTitle}</h2>\n`
       : "";
     return title + `<${token.tag} class="list-of-figures-list">\n`;
   };
 }
 
+function figure_wrapper_renderer(opts, defaultRenderer) {
+  if (!opts.wrap || !opts.wrapTag) return defaultRenderer;
+  return (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const id = token.meta.targetId;
+    const entry = env[opts.options.ns].refs[id];
+    const rId = new RegExp('\\s?id="' + id + '"');
+    const figure = defaultRenderer(tokens, idx, options, env, self).replace(rId, "");
+    if (id && entry) {
+      return (
+        `<${opts.wrapTag} id="${id}" ${opts.wrapClass ? `class="${opts.wrapClass}"` : ""}>\n` +
+        `  ${figure}\n` +
+        `</${opts.wrapTag}>`
+      );
+    }
+  };
+}
+
 plugin.defaults = {
-  listTitle: "List of Figures",
+  listHeaderAnchor: true,
+  wrap: true,
+  wrapTag: "div",
+  wrapClass: "wrapper",
+  options: require("markdown-it-figure-references").defaults,
 };
 
 module.exports = plugin;

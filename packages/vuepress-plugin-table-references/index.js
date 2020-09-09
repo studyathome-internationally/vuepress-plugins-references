b7395f7aac68b4866af7e3687801e9aa7d0f3982
Alija Sabic <sabic.alija@gmail.com>
2020-09-09 02:28:36 +0200

    vuepress-plugin-table-references: add custom figure_wrapper, add new options and pass-through handling

--- packages/vuepress-plugin-table-references/index.js
+++ packages/vuepress-plugin-table-references/index.js
@@ -1,10 +1,17 @@
 const plugin = (options, ctx) => {
   const opts = Object.assign({}, plugin.defaults, options);
+  opts.options = Object.assign({}, require("markdown-it-table-references").defaults, options.options);
   return {
     name: "vuepress-plugin-table-references",
     extendMarkdown: (md) => {
-      md.use(require("markdown-it-table-references"), opts);
+      md.use(require("markdown-it-table-references"), opts.options);
       md.renderer.rules.table_reference_list_open = table_reference_list_open_renderer(opts);
+
+      const defaultOpenRenderer = md.renderer.rules.table_wrapper_open;
+      md.renderer.rules.table_wrapper_open = table_wrapper_open_renderer(opts, defaultOpenRenderer);
+
+      const defaultCloseRenderer = md.renderer.rules.table_wrapper_close;
+      md.renderer.rules.table_wrapper_close = table_wrapper_close_renderer(opts, defaultCloseRenderer);
     },
   };
 };
@@ -12,15 +19,42 @@ const plugin = (options, ctx) => {
 function table_reference_list_open_renderer(opts) {
   return (tokens, idx /* , options, env, self */) => {
     const token = tokens[idx];
-    const title = opts.listTitle
-      ? `<h2 id="list-of-tables"><a href="#list-of-tables" class="header-anchor">#</a>${opts.listTitle}</h2>\n`
+    const title = opts.options.listTitle
+      ? `<h2 id="list-of-tables">${
+          opts.listHeaderAnchor ? `<a href="#list-of-tables" class="header-anchor">#</a>` : ""
+        }${opts.options.listTitle}</h2>\n`
       : "";
     return title + `<${token.tag} class="list-of-tables-list">\n`;
   };
 }
 
+function table_wrapper_open_renderer(opts, defaultRenderer) {
+  if (!opts.wrap || !opts.wrapTag) return defaultRenderer;
+  return (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const id = token.attrGet("id");
+    const rId = new RegExp('\\s?id="' + id + '"');
+    const figureOpen = defaultRenderer(tokens, idx, options, env, self).replace(rId, "");
+    if (id) {
+      return `<${opts.wrapTag} id="${id}"${opts.wrapClass ? ` class="${opts.wrapClass}"` : ""}>${figureOpen}`;
+    }
+  };
+}
+
+function table_wrapper_close_renderer(opts, defaultRenderer) {
+  if (!opts.wrap || !opts.wrapTag) return defaultRenderer;
+  return (tokens, idx, options, env, self) => {
+    const figureClose = defaultRenderer(tokens, idx, options, env, self);
+    return figureClose + `</${opts.wrapTag}>\n`;
+  };
+}
+
 plugin.defaults = {
-  listTitle: "List of Tables",
+  listHeaderAnchor: true,
+  wrap: true,
+  wrapTag: "div",
+  wrapClass: "wrapper",
+  options: {},
 };
 
 module.exports = plugin;

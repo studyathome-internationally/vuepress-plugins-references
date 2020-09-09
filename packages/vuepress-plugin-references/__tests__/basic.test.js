2114c13e350527a89a8f6727fee5194265329a7b
Alija Sabic <sabic.alija@gmail.com>
2020-09-09 03:46:06 +0200

    vuepress-plugin-references: update snapshots

--- packages/vuepress-plugin-references/__tests__/basic.test.js
+++ packages/vuepress-plugin-references/__tests__/basic.test.js
@@ -24,18 +24,18 @@ describe("basic functionality", () => {
       frontmatter: data,
     });
     expect(html).toMatchInlineSnapshot(`
-      <h1>References</h1>
+      <h1 id="references"><a class="header-anchor" href="#references">#</a> References</h1>
       <p>
-      <div id="the-stormtroopocat" class="figure-wrapper">
+      <div id="the-stormtroopocat" class="wrapper">
         <figure>
-          <img src="https://octodex.github.com/images/stormtroopocat.jpg" alt="Stormtroopocat" title="The Stormtroopocat" id="the-stormtroopocat-img" />
+          <img src="https://octodex.github.com/images/stormtroopocat.jpg" alt="Stormtroopocat" title="The Stormtroopocat">
           <figcaption>
             <a href="#the-stormtroopocat">Figure 1</a>: The Stormtroopocat
           </figcaption>
         </figure>
       </div>
       </p>
-      <div class="table-wrapper" id="client-overview">
+      <div id="client-overview" class="wrapper">
         <figure>
           <table>
             <thead>
@@ -60,25 +60,27 @@ describe("basic functionality", () => {
           </figcaption>
         </figure>
       </div>
-      <div id="wikipedia-authors-markdown" class="attribution-container">
-        <p>
-          <a href="#wikipedia-authors-markdown" class="attribution-anchor">#</a><span>Based on: <a href="https://en.wikipedia.org/w/index.php?title=Markdown&oldid=975764292">Markdown</a> by <a href="https://en.wikipedia.org/w/index.php?title=Markdown&action=history">Wikipedia Authors</a>, License: <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons: Attribution-ShareAlike 4.0</a></span>
-        </p>
+      <div id="wikipedia-authors-markdown" class="wrapper">
         <div>
-          <p>Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
-            Markdown is often used for formatting readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.</p>
+          <div>
+            <p>Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+              Markdown is often used for formatting readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.</p>
+          </div>
+          <p>
+            <span>Based on: <a href="https://en.wikipedia.org/w/index.php?title=Markdown&oldid=975764292">Markdown</a> by <a href="https://en.wikipedia.org/w/index.php?title=Markdown&action=history">Wikipedia Authors</a>, License: <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons: Attribution-ShareAlike 4.0</a></span>
+          </p>
         </div>
       </div>
       <p>The example contains following references: <a href="#the-stormtroopocat">Figure 1</a>, <a href="#client-overview">Table 1</a>, <a href="#wikipedia-authors-markdown">Attribution 1</a>.</p>
-      <h2 id="list-of-figures">List of Figures</h2>
+      <h2 id="list-of-figures"><a href="#list-of-figures" class="header-anchor">#</a>List of Figures</h2>
       <ol class="list-of-figures-list">
         <li><a href="#the-stormtroopocat">Figure 1</a>: The Stormtroopocat</li>
       </ol>
-      <h2 id="list-of-tables">List of Tables</h2>
+      <h2 id="list-of-tables"><a href="#list-of-tables" class="header-anchor">#</a>List of Tables</h2>
       <ol class="list-of-tables-list">
         <li><a href="#client-overview">Table 1</a>: Client overview</li>
       </ol>
-      <h2 id="list-of-attributions">List of Attributions</h2>
+      <h2 id="list-of-attributions"><a href="#list-of-attributions" class="header-anchor">#</a>List of Attributions</h2>
       <ol class="list-of-attributions-list">
         <li><a href="#wikipedia-authors-markdown">Attribution 1</a>: Markdown</li>
       </ol>

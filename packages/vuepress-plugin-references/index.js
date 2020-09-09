5f836fa78e60a7b72c505729216eba94817aee6d
Alija Sabic <sabic.alija@gmail.com>
2020-09-09 04:15:23 +0200

    vuepress-plugin-references: fix option pass-through

--- packages/vuepress-plugin-references/index.js
+++ packages/vuepress-plugin-references/index.js
@@ -1,14 +1,18 @@
 const plugin = (options, ctx) => {
   const opts = Object.assign({}, plugin.defaults.references, options.references);
-  const optsFigures = Object.assign({}, plugin.defaults.figures, options.figures);
-  const optsTables = Object.assign({}, plugin.defaults.tables, options.tables);
-  const optsAttributions = Object.assign({}, plugin.defaults.attributions, options.attributions);
+  const optsFigures = Object.assign({}, require("markdown-it-figure-references").defaults, options.figures);
+  const optsTables = Object.assign({}, require("markdown-it-table-references").defaults, options.tables);
+  const optsAttributions = Object.assign(
+    {},
+    require("markdown-it-attribution-references").defaults,
+    options.attributions
+  );
   return {
     name: "vuepress-plugin-references",
     plugins: [
-      ["figure-references", optsFigures],
-      ["table-references", optsTables],
-      ["attribution-references", optsAttributions],
+      ["figure-references", { options: optsFigures }],
+      ["table-references", { options: optsTables }],
+      ["attribution-references", { options: optsAttributions }],
     ],
     extendMarkdown: (md) => {
       md.use(require("markdown-it-references"), opts);

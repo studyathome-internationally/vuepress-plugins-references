cba825f085c029fc060b06bc08e2c99d9c6543d9
Alija Sabic <sabic.alija@gmail.com>
2020-09-12 21:02:17 +0200

    vuepress-plugin-attribution-reference: update markdown-it-attribution-references (alpha.10), add custom wrapper for vuepress, update usage instruction, document new configuration properties

--- packages/vuepress-plugin-attribution-references/README.md
+++ packages/vuepress-plugin-attribution-references/README.md
@@ -12,8 +12,8 @@ or
 
 ```js
 module.exports = {
-  plugins: ["vuepress-plugin-attribution-references"]
-}
+  plugins: ["vuepress-plugin-attribution-references"],
+};
 ```
 
 or
@@ -28,21 +28,45 @@ Enable plugin in `.vuepress/config.js`
 
 ```js
 module.exports = {
-  plugins: ["table-attribution"]
-}
+  plugins: ["table-attribution"],
+};
 ```
 
 or
 
 ```js
 module.exports = {
-  plugins: [["table-attribution", opts]]
-}
+  plugins: [["table-attribution", opts]],
+};
 ```
 
-The `opts` object is passed through to [markdown-it-attribution-references](https://www.npmjs.com/package/markdown-it-attribution-references) (as is).
+<style>
+table { width: 100%;} td:first-child {width: 15%;} td:last-child {width: 45%;}
+</style>
+
+The `opts` object can contain:
+
+| Name      | Description                                                                                                              | Default                                                                                                                                |
+| --------- | ------------------------------------------------------------------------------------------------------------------------ | -------------------------------------------------------------------------------------------------------------------------------------- |
+| `wrap`    | Rule name to insert new rules after.                                                                                     | see below                                                                                                                              |
+| `options` | [markdown-it-attribution-references](https://www.npmjs.com/package/markdown-it-figure-references) configuration options. | see [here](https://github.com/studyathome-internationally/markdown-it-plugins/tree/master/packages/markdown-it-attribution-references) |
+
+<br/>
+
+The `wrap` object can contain:
+
+| Name     | Description    | Default     |
+| -------- | -------------- | ----------- |
+| `enable` | Wrap figure.   | `true`      |
+| `tag`    | Wrapper tag.   | `"div"`     |
+| `class`  | Wrapper class. | `"wrapper"` |
+
+<br/>
 
 ## License
 
 [GPL-3.0](https://github.com/studyathome-internationally/vuepress-plugins/blob/master/LICENSE) &copy; [StudyATHome Internationally](https://github.com/studyathome-internationally/)
+
+```
+
 ```
--- packages/vuepress-plugin-attribution-references/index.js
+++ packages/vuepress-plugin-attribution-references/index.js
@@ -1,62 +1,50 @@
 const plugin = (options, ctx) => {
-  const opts = Object.assign({}, plugin.defaults, options);
-  opts.options = Object.assign({}, require("markdown-it-attribution-references").defaults, options.options);
+  const opts = loadOptions(options);
+  opts.options.after = "inline";
   return {
     name: "vuepress-plugin-attribution-references",
     extendMarkdown: (md) => {
       md.use(require("markdown-it-attribution-references"), opts.options);
-      md.renderer.rules.attribution_list_open = attribution_list_open_renderer(opts);
 
-      const defaultOpenRenderer = md.renderer.rules.attribution_open;
-      md.renderer.rules.attribution_open = attribution_open_renderer(opts, defaultOpenRenderer);
-
-      const defaultCloseRenderer = md.renderer.rules.attribution_close;
-      md.renderer.rules.attribution_close = attribution_close_renderer(opts, defaultCloseRenderer);
+      if (opts.wrap.enable) {
+        md.renderer.rules.attribution_parent_open = attribution_parent_open_renderer(opts);
+        md.renderer.rules.attribution_parent_close = attribution_parent_close_renderer(opts);
+      }
     },
   };
 };
 
-function attribution_list_open_renderer(opts) {
-  return (tokens, idx /* , options, env, self */) => {
-    const token = tokens[idx];
-    const title = opts.options.listTitle
-      ? `<h2 id="list-of-attributions">${
-          opts.listHeaderAnchor ? `<a href="#list-of-attributions" class="header-anchor">#</a>` : ""
-        }${opts.options.listTitle}</h2>\n`
-      : "";
-    return title + `<${token.tag} class="list-of-attributions-list">\n`;
-  };
-}
-
-function attribution_open_renderer(opts, defaultRenderer) {
-  if (!opts.wrap || !opts.wrapTag) return defaultRenderer;
+function attribution_parent_open_renderer(opts) {
   return (tokens, idx, options, env, self) => {
     const token = tokens[idx];
     const id = token.attrGet("id");
-    const attributionOpen = defaultRenderer(tokens, idx, options, env, self);
-    if (id) {
-      const rId = new RegExp('\\s?id="' + id + '"');
-      return `<${opts.wrapTag} id="${id}"${
-        opts.wrapClass ? ` class="${opts.wrapClass}"` : ""
-      }>${attributionOpen.replace(rId, "")}`;
-    }
-    return attributionOpen;
+    const className = token.attrGet("class");
+    return `<${opts.wrap.tag} id="${id}" class="${opts.wrap.class}">\n<${token.tag} class="${className}">\n`;
   };
 }
 
-function attribution_close_renderer(opts, defaultRenderer) {
-  if (!opts.wrap || !opts.wrapTag) return defaultRenderer;
+function attribution_parent_close_renderer(opts) {
   return (tokens, idx, options, env, self) => {
-    const attributionClose = defaultRenderer(tokens, idx, options, env, self);
-    return attributionClose + `</${opts.wrapTag}>\n`;
+    const token = tokens[idx];
+    return `</${token.tag}>\n</${opts.wrap.tag}>`;
   };
+}
+
+function loadOptions(options) {
+  return options
+    ? {
+        wrap: Object.assign({}, plugin.defaults.wrap, options.wrap ? options.wrap : {}),
+        options: Object.assign({}, plugin.defaults.options, options.options ? options.options : {}),
+      }
+    : plugin.defaults;
 }
 
 plugin.defaults = {
-  listHeaderAnchor: true,
-  wrap: true,
-  wrapTag: "div",
-  wrapClass: "wrapper",
+  wrap: {
+    enable: true,
+    tag: "div",
+    class: "wrapper",
+  },
   options: {},
 };
 

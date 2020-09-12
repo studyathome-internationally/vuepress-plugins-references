3ba3d347ed81bc6d005b5984f45ec87491ecfa0e
Alija Sabic <sabic.alija@gmail.com>
2020-09-12 22:48:12 +0200

    vuepress-plugin-table-references: fix option loading assertion

--- packages/vuepress-plugin-table-references/index.js
+++ packages/vuepress-plugin-table-references/index.js
@@ -31,7 +31,7 @@ function figure_table_close_renderer(opts) {
 
 function loadOptions(options) {
   return {
-    wrap: Object.assign({}, plugin.defaults.wrap, typeof options.wrap === "boolean" ? options.wrap : {}),
+    wrap: Object.assign({}, plugin.defaults.wrap, options.wrap ? options.wrap : {}),
     options: Object.assign({}, plugin.defaults.options, options.options ? options.options : {}),
   };
 }

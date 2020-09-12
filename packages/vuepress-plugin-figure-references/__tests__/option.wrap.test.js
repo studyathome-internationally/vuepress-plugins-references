0ad8c17e122a0c35b7e39208d3c43195a8421112
Alija Sabic <sabic.alija@gmail.com>
2020-09-12 22:50:02 +0200

    vuepress-plugin-figure-referenes: add test suites and cases, update test coverage reports and badges

---
+++ packages/vuepress-plugin-figure-references/__tests__/option.wrap.test.js
@@ -0,0 +1,56 @@
+const { resolve, join } = require("path");
+const { readFileSync } = require("fs");
+
+const { createApp } = require("@vuepress/core");
+const { parseFrontmatter } = require("@vuepress/shared-utils");
+
+describe("options", () => {
+  let app;
+
+  beforeAll(async () => {
+    const temp = resolve(__dirname, ".temp");
+    app = createApp({
+      temp,
+      plugins: [
+        [
+          "vuepress-plugin-figure-references",
+          {
+            wrap: {
+              enable: false,
+            },
+            options: {
+              list: {
+                title: "List of Images",
+              },
+            },
+          },
+        ],
+      ],
+    });
+    return app.process();
+  });
+
+  it("option: wrap", () => {
+    const text = readFileSync(join(__dirname, "__cases__", "basic.2.md"), "utf8");
+    const { data, content } = parseFrontmatter(text);
+    const { html } = app.markdown.render(content, {
+      ...(data.ENV || {}),
+      frontmatter: data,
+    });
+    expect(html).toMatchInlineSnapshot(`
+      <h1 id="hello-world"><a class="header-anchor" href="#hello-world">#</a> Hello World</h1>
+      <p>
+      <figure id="the-stormtroopocat">
+        <img src="https://octodex.github.com/images/stormtroopocat.jpg" alt="Stormtroopocat" title="The Stormtroopocat">
+        <figcaption>
+          <a href="#the-stormtroopocat" class="anchor">§</a><a href="#the-stormtroopocat" class="label">Figure 1</a>: The Stormtroopocat
+        </figcaption>
+      </figure>
+      </p>
+      <h2 id="list-of-figures" class="list"><a class="header-anchor" href="#list-of-figures">#</a> List of Images</h2>
+      <ol class="list">
+        <li class="item"><a href="#the-stormtroopocat" class="label">Figure 1</a>: The Stormtroopocat</li>
+      </ol>
+    `);
+  });
+});
